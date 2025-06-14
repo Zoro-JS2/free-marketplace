81811ccba558845bd9747096d5e6baca5c21d81f
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 <dema2000g@gmail.com>
2025-06-14 10:49:55 +0300

    update  confirm order for authed users

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../nav/nav.component';
 import { CommonModule } from '@angular/common';
+import { LoginDialogService } from '../../login-dialog.service';
 
 @Component({
   selector: 'app-order',
@@ -10,15 +11,30 @@ import { CommonModule } from '@angular/common';
 })
 export class OrderComponent {
   selectedProducts: any[] = [];
+  visible: boolean = false;
+
+  constructor(private loginDialogService: LoginDialogService) {}
 
   ngOnInit(): void {
     const stored = localStorage.getItem('selectedProducts');
     this.selectedProducts = stored ? JSON.parse(stored) : [];
+
+    this.checkUser();
+  }
+  checkUser() {
+    const user = localStorage.getItem('user');
+    if (user) {
+      this.visible = true;
+    }
   }
   clearCart() {
-    localStorage.removeItem('selectedProducts');
-    this.selectedProducts = [];
-    window.location.reload();
+    if (this.visible == true) {
+      localStorage.removeItem('selectedProducts');
+      this.selectedProducts = [];
+      window.location.reload();
+    } else {
+      this.loginDialogService.openLoginDialog();
+    }
   }
   confirmDelete(index: number): void {
     const confirmed = window.confirm(
